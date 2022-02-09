42145419a6dbda432c318cb855c1120db371aa61
Alexis Mansilla Ruiz <alexis.mansilla04@gmail.com>
2022-02-08 23:17:09 -0300

    Add mutation

--- src/components/ListMemes.vue
+++ src/components/ListMemes.vue
@@ -1,21 +1,26 @@
 <template>
-  <h1> {{ title }} </h1>
+  <div>
+    <h1> {{ title }} </h1>
+    <p>{{ memes }}</p>
+  </div>
 </template>
 
 <script>
-import { onMounted } from "vue"
+import { onMounted, computed } from "vue"
 import { useStore } from "vuex"
 
 export default {
   setup() {
     const store = useStore()
+    const memes = computed(() => store.state.memes)
 
     onMounted(() => {
       store.dispatch("getMemes")
     })
 
     return {
-      title: store.state.titleApp
+      title: store.state.titleApp,
+      memes
     }
   }
 }
